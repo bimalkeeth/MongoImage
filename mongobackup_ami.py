9f14215dc13bf63afdd375e4ca1e0d8f80b33374
Bimal Kaluarachchi <bimalkeeth@gmail.com>
2019-11-12 15:14:34 +1100

    Initial commit

---
+++ mongobackup_ami.py
@@ -0,0 +1,60 @@
+import boto3
+from datetime import datetime, timezone ,timedelta
+import dateutil
+
+
+def days_diff(d1, d2):
+    return abs((d2 - d1).days)
+
+
+if __name__ == '__main__':
+    client = boto3.client('ec2')
+
+    custom_imagefiler = [
+        {
+            'Name': 'name',
+            'Values': ['*jenkinsvpc-mongo*']
+        }
+    ]
+
+    imageResult = client.describe_images(Filters=custom_imagefiler)
+    print(imageResult)
+    for image in imageResult['Images']:
+
+        date = dateutil.parser.parse(image['CreationDate'])
+        date.replace(tzinfo=None)
+        todayDate = datetime.now(timezone.utc)
+
+        dateDiff = days_diff(date, todayDate)
+        if dateDiff > 5:
+            response = client.deregister_image(
+                ImageId=image['ImageId'],
+                DryRun=True
+            )
+        else:
+            print(image['ImageId'] + '-' + 'is still valid')
+
+    custom_filter = [{
+        'Name': 'tag:Name',
+        'Values': ['*vpc-mongo*']
+    },
+        {
+            'Name': 'tag:awsbackup',
+            'Values': ['yes']
+        },
+        {
+            'Name': 'instance-state-name',
+            'Values': ['running']
+        }]
+    result = client.describe_instances(Filters=custom_filter)
+    for item in result['Reservations']:
+
+        name = ''
+        for instance in item['Instances']:
+
+            for tag in instance['Tags']:
+                if tag['Key'] != '':
+                    name = tag['Value']
+                    break
+            amiName = "jenkins" + name + "-" + datetime.strftime(datetime.utcnow(), '%Y%m%d%H%M%S')
+            client.create_image(InstanceId=instance['InstanceId'], Name=amiName, NoReboot=True)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name='MongoImage',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='bimal.kaluarachchi',
+    author_email='',
+    description=''
+)
